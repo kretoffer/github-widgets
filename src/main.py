1a6a3889f82ca4e127ec7cb4e3f461b8b48e6c4d
Kretov Artem <20kretovartem000@gmail.com>
2026-08-05 21:31:59 +0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello world"
+
+
+def main():
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
