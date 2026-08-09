563a5a9fa1f4e7400c856386aa40975d87c1fc01
Kretov Artem <20kretovartem000@gmail.com>
2026-08-09 18:47:11 +0300

    add favicon

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 from api import api_roter
 
 app = FastAPI()
 
 app.include_router(api_roter)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.ico")
 
 
 def main():
